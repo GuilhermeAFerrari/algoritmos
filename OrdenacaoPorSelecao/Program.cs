a6c37f71a0277667d450033d5e6b964d1d4869ad
Guilherme Ferrari <guile.ferrari@hotmail.com>
2025-12-25 19:38:28 -0300

    feat: implemented selection sort

---
+++ OrdenacaoPorSelecao/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] numeros = { 12, 3, 4, 78, 5, 2, 56, 5, 1, 99 };
+
+Ordenar(numeros);
+
+Console.WriteLine($"Array ordenado:");
+foreach (var item in numeros)
+{
+    Console.WriteLine(item);
+}
+
+static void Ordenar(int[] numeros)
+{
+    for (int i = 0; i < numeros.Length - 1; i++)
+    {
+        var menorIndice = i;
+
+        for (int j = i + 1; j < numeros.Length; j++)
+        {
+            if (numeros[j] < numeros[menorIndice])
+            {
+                menorIndice = j;
+            }
+        }
+
+        if (menorIndice != i)
+        {
+            int temp = numeros[i];
+            numeros[i] = numeros[menorIndice];
+            numeros[menorIndice] = temp;
+
+            //(numeros[menorIndice], numeros[i]) = (numeros[i], numeros[menorIndice]); // tuple Swap
+        }
+    }
+}
