fd957792af1ff036d2da02252c77826117c79494
Guilherme Ferrari <guile.ferrari@hotmail.com>
2026-02-01 15:19:57 -0300

    feat: implemented grafos

---
+++ Dijkstra/Program.cs
@@ -0,0 +1,77 @@
+﻿var grafo = new GrafoDijkstra();
+
+grafo.AddAresta(0, 1, 1);
+grafo.AddAresta(0, 2, 10);
+grafo.AddAresta(1, 3, 1);
+grafo.AddAresta(2, 3, 1);
+grafo.AddAresta(3, 4, 1);
+
+var caminhoDijkstra = grafo.MenorCaminhoDijkstra(0, 4);
+
+Console.WriteLine("Dijkstra: " + string.Join(" -> ", caminhoDijkstra));
+
+
+public class GrafoDijkstra
+{
+    private readonly Dictionary<int, List<(int destino, int peso)>> _adj = new();
+
+    public void AddAresta(int origem, int destino, int peso)
+    {
+        if (!_adj.ContainsKey(origem))
+            _adj[origem] = new List<(int, int)>();
+
+        _adj[origem].Add((destino, peso));
+    }
+
+    public List<int> MenorCaminhoDijkstra(int inicio, int destino)
+    {
+        var dist = new Dictionary<int, int>();
+        var pai = new Dictionary<int, int>();
+        var pq = new PriorityQueue<int, int>();
+
+        foreach (var v in _adj.Keys)
+            dist[v] = int.MaxValue;
+
+        dist[inicio] = 0;
+        pq.Enqueue(inicio, 0);
+
+        while (pq.Count > 0)
+        {
+            int atual = pq.Dequeue();
+
+            if (atual == destino)
+                return Reconstruir(pai, inicio, destino);
+
+            foreach (var (vizinho, peso) in _adj[atual])
+            {
+                int novaDist = dist[atual] + peso;
+
+                if (novaDist < dist.GetValueOrDefault(vizinho, int.MaxValue))
+                {
+                    dist[vizinho] = novaDist;
+                    pai[vizinho] = atual;
+                    pq.Enqueue(vizinho, novaDist);
+                }
+            }
+        }
+
+        return new List<int>();
+    }
+
+    private List<int> Reconstruir(
+        Dictionary<int, int> pai, int inicio, int destino)
+    {
+        var caminho = new List<int>();
+        int atual = destino;
+
+        while (atual != inicio)
+        {
+            caminho.Add(atual);
+            atual = pai[atual];
+        }
+
+        caminho.Add(inicio);
+        caminho.Reverse();
+        return caminho;
+    }
+}
---
+++ PesquisaEmLargura/Program.cs
@@ -0,0 +1,75 @@
+﻿var grafo = new GrafoBfs();
+
+grafo.AddAresta(0, 1);
+grafo.AddAresta(0, 2);
+grafo.AddAresta(1, 3);
+grafo.AddAresta(1, 4);
+grafo.AddAresta(2, 3);
+grafo.AddAresta(3, 4);
+
+var caminhoBfs = grafo.MenorCaminhoBfs(0, 4);
+
+Console.WriteLine("BFS: " + string.Join(" -> ", caminhoBfs));
+
+class GrafoBfs
+{
+    private readonly Dictionary<int, List<int>> _adj = new();
+
+    public void AddAresta(int origem, int destino)
+    {
+        if (!_adj.ContainsKey(origem))
+            _adj[origem] = new List<int>();
+
+        _adj[origem].Add(destino);
+    }
+
+    public List<int> MenorCaminhoBfs(int inicio, int destino)
+    {
+        var fila = new Queue<int>();
+        var visitado = new HashSet<int>();
+        var pai = new Dictionary<int, int>();
+
+        fila.Enqueue(inicio);
+        visitado.Add(inicio);
+
+        while (fila.Count > 0)
+        {
+            int atual = fila.Dequeue();
+
+            if (atual == destino)
+                return Reconstruir(pai, inicio, destino);
+
+            if (!_adj.ContainsKey(atual))
+                continue;
+
+            foreach (var vizinho in _adj[atual])
+            {
+                if (!visitado.Contains(vizinho))
+                {
+                    visitado.Add(vizinho);
+                    pai[vizinho] = atual;
+                    fila.Enqueue(vizinho);
+                }
+            }
+        }
+
+        return new List<int>();
+    }
+
+    private List<int> Reconstruir(
+        Dictionary<int, int> pai, int inicio, int destino)
+    {
+        var caminho = new List<int>();
+        int atual = destino;
+
+        while (atual != inicio)
+        {
+            caminho.Add(atual);
+            atual = pai[atual];
+        }
+
+        caminho.Add(inicio);
+        caminho.Reverse();
+        return caminho;
+    }
+}
